71206f6ffc8eee83a4c4958cab0d499d4ed580e2
huangchengdu <394042583@qq.com>
2016-06-03 17:52:48 +0800

    no message

---
+++ javascript高级程序设计代码/chapter3.one.js
@@ -0,0 +1,11 @@
+// var message = "some string";
+// console.log(typeof message);
+// console.log(typeof(message));
+// console.log(typeof 95);
+// console.log(typeof null);
+
+
+var message;
+console.log(message == undefined);
+console.log(typeof message);
+console.log(typeof age);//没有声明的变量
