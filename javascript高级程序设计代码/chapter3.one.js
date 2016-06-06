8bb719215911ed1fff4aa2beb8531cbb2abe817e
huangchengdu <394042583@qq.com>
2016-06-06 10:05:29 +0800

    commit

--- javascript高级程序设计代码/chapter3.one.js
+++ javascript高级程序设计代码/chapter3.one.js
@@ -5,7 +5,24 @@
 // console.log(typeof null);
 
 
-var message;
-console.log(message == undefined);
-console.log(typeof message);
-console.log(typeof age);//没有声明的变量
+// var message;
+// console.log(message == undefined);
+// console.log(typeof message);
+// console.log(typeof age);//没有声明的变量
+
+
+// var car = null;
+// console.log(typeof car);
+// console.log(null == undefined);
+
+// var result = Number.MAX_VALUE + Number.MAX_VALUE;
+// console.log(isFinite(result));
+// console.log(NaN == NaN);
+// console.log(isNaN(NaN));
+// console.log(isNaN("10"));
+// console.log(isNaN("blue"));
+
+console.log(Number("hello world!"));
+console.log(Number(""));
+console.log(Number("000011"));
+console.log(Number(true));
