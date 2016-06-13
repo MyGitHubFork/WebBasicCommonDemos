1c560ea24e844a7875293991c798f749020911b6
huangchengdu <394042583@qq.com>
2016-06-13 08:26:36 +0800

    update

--- javascript高级程序设计代码/chapter6.three.继承.js
+++ javascript高级程序设计代码/chapter6.three.继承.js
@@ -47,6 +47,129 @@
 // console.log(instance.age);
 
 //=============================组合继承====================
+// function SuperType(name){
+// 	this.name = name;
+// 	this.colors = ["red", "blue", "green"];
+// }
+// SuperType.prototype.sayName = function(){
+// 	console.log(this.name);
+// };
+// function SubType(name, age){
+// 	SuperType.call(this, name);
+// 	this.age = age;
+// }
+// SubType.prototype = new SuperType();
+// SubType.prototype.constructor = SubType;
+// SubType.prototype.sayAge = function(){
+// 	console.log(this.age);
+// };
+// var instance1 = new SubType("Nicholas", 29);
+// instance1.colors.push("black");
+// console.log(instance1.colors);
+// instance1.sayName();
+// instance1.sayAge();
+
+// var instance2 = new SubType("Greg", 27);
+// console.log(instance2.colors);
+// instance2.sayName();
+// instance2.sayAge();
+
+
+//=============================原型式继承====================
+// var person = {
+// 	name: "Nicholas",
+// 	friends: ["Shelby", "Court", "Van"]
+// };
+// var anotherPerson = Object.create(person,{
+// 	name:{
+// 		value: "Greg"
+// 	}
+// });
+// console.log(anotherPerson.name);
+
+//=============================寄生式继承====================
+// function object(o){
+// 	function F(){}
+// 	F.prototype = o;
+// 	return new F();
+// }
+// function createAnother(original){
+// 	var clone = object(original);
+// 	clone.sayHi = function(){
+// 		console.log("hi");
+// 	}
+// 	return clone;
+// }
+// var person = {
+//     name: "Nicholas",
+//     friends: ["Shelby", "Court", "Van"]
+// };
+// var anotherPerson1 = createAnother(person);
+// anotherPerson1.friends.push("Van");
+// console.log(anotherPerson1.friends);
+// anotherPerson1.sayHi(); 
+// var anotherPerson2 = createAnother(person);
+// console.log(anotherPerson2.friends);
+// anotherPerson2.sayHi(); 
+
+//=============================寄生组合式继承====================
+function object(o){
+	function F(){}
+	F.prototype = o;
+	return new F();
+}
+function inheritPrototype(subType, superType){
+	var prototype = object(superType.prototype);
+	prototype.constructor = subType;
+	subType.prototype =prototype;
+}
+function SuperType(name){
+    this.name = name;
+    this.colors = ["red", "blue", "green"];
+}
+SuperType.prototype.sayName = function(){
+    console.log(this.name);
+};
+function SubType(name, age){
+    SuperType.call(this, name);
+    this.age = age;
+}
+inheritPrototype(SubType, SuperType);
+SubType.prototype.sayAge = function(){
+    console.log(this.age);
+   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
