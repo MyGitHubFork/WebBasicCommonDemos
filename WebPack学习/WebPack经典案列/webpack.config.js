024c8407602749c63feb7bad4cadcbc7eb0d3c99
huangchengdu <394042583@qq.com>
2016-09-29 11:53:30 +0800

    webpack.config.js添加

---
+++ WebPack学习/WebPack经典案列/webpack.config.js
@@ -0,0 +1,14 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: 'style!css'}
+    ]
+  }
+}
