c3e6101cb5709ab8afb8ac034ac27a39cc5d70f8
黄成都 <394042583@qq.com>
2016-09-30 08:28:27 +0800

    update

--- vuejs入门基础/my-second-vue-project/src/App.vue
+++ vuejs入门基础/my-second-vue-project/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
       <h1 v-text="title"></h1>
+      <input v-model="newItem" v-on:keyup.enter="addNew">
       <ul>
-          <li v-for="item in items" v-bind:class="{finished:item.isFinished}">
+          <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">
               {{item.label}}
           </li>
       </ul>
@@ -10,6 +11,8 @@
 </template>
 
 <script>
+import Store from './store'
+
 
 export default {
     data:function(){
@@ -17,12 +20,24 @@ export default {
             title:'this is a todo list',
             items:[
             {label:'coding',isFinished:false
-        },{
+            },{
             label:'walking',isFinished:true
             }],
+            newItem:'',
             liClass:'thisisliClass'
         };
-       
+    },
+    methods:{
+        toggleFinish:function(item){
+            item.isFinished = !item.isFinished;
+        },
+        addNew:function(){
+            this.items.push({
+                label:this.newItem,
+                isFinished:false,
+            });
+            this.newItem = '';
+        }
     }
 }
 </script>
