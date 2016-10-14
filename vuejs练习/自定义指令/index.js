c1ba26eee399318b38cdaf5f836c9eab502bce61
huangchengdu <394042583@qq.com>
2016-10-14 16:25:14 +0800

    update

---
+++ vuejs练习/自定义指令/index.js
@@ -0,0 +1,19 @@
+Vue.directive('demo', {
+	bind: function () {
+		console.log('demo bound!')
+	},
+	update: function (value) {
+		this.el.innerHTML =
+			'name - '       + this.name + '<br>' +
+			'expression - ' + this.expression + '<br>' +
+			'argument - '   + this.arg + '<br>' +
+			'modifiers - '  + JSON.stringify(this.modifiers) + '<br>' +
+			'value - '      + value
+	}
+})
+var demo = new Vue({
+	el: '#demo',
+	data: {
+		msg: 'hello!'
+	}
+})
