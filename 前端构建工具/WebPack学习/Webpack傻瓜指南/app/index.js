3ff02289f786a0dd6707aa8f8602b1300c99b0aa
huangchengdu <394042583@qq.com>
2016-11-15 16:45:32 +0800

    添加第三方库支持。比如jquery

--- 前端构建工具/WebPack学习/Webpack傻瓜指南/app/index.js
+++ 前端构建工具/WebPack学习/Webpack傻瓜指南/app/index.js
@@ -2,14 +2,20 @@
  * @Author: huangchengdu
  * @Date:   2016-11-15 15:45:19
  * @Last Modified by:   huangchengdu
- * @Last Modified time: 2016-11-15 16:31:57
+ * @Last Modified time: 2016-11-15 16:44:57
  */
 
 var sub = require('./sub');
 //require('./main.css');
 require('./main.scss');
 
+var $ = require('jquery');
+var moment = require('moment');
+
 var app = document.createElement('div');
 app.innerHTML = '<h1>Hello World test</h1>';
 app.appendChild(sub());
 document.body.appendChild(app);
+
+$('body').append('<p>look at me! now is ' + moment().format() + '</p>');
+
