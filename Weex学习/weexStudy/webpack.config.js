88140a6f9438713a842a2860b31036cd83b9e088
huangchengdu <huang2009303513@gmail.com>
2017-02-28 11:39:34 +0800

    update

---
+++ Weex学习/weexStudy/webpack.config.js
@@ -0,0 +1,43 @@
+require('webpack');
+require('weex-loader');
+var path = require('path');
+var fs = require('fs');
+var entry = {};
+
+function walk(dir, root) {
+  var directory = path.join(__dirname, root, dir)
+  fs.readdirSync(directory).forEach(function (file) {
+      var fullpath = path.join(directory, file)
+      var stat = fs.statSync(fullpath)
+        // support for vue file
+      if (stat.isFile() && (path.extname(fullpath) === '.we' || path.extname(fullpath) === '.vue')) {
+          var name = path.join(dir, path.basename(file, '.we'))
+          entry[name] = fullpath + '?entry=true'
+        }
+        else if (stat.isDirectory()) {
+          var subdir = path.join(dir, file)
+          walk(subdir, root)
+        }
+      })
+  }
+  walk('./', 'src');
+  module.exports = {
+    entry: entry
+    , output: {
+      path: 'dist'
+      , filename: '[name].js'
+    }
+    , devtool: 'inline-source-map'
+    , module: {
+      loaders: [
+        {
+          test: /\.we(\?[^?]+)?$/
+          , loaders: ['weex-loader']
+      }
+        , {
+          test: /\.vue(\?[^?]+)?$/
+          , loaders: ['vue']
+      }
+    ]
+    }
+  }
