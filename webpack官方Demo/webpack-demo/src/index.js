1e9e5c6a4d73815d5f781bcf1f2e6f4388cd47ed
xibeifeng <394042583@qq.com>
2017-07-05 17:31:33 +0800

    添加script命令和样式表

--- webpack官方Demo/webpack-demo/src/index.js
+++ webpack官方Demo/webpack-demo/src/index.js
@@ -1,9 +1,10 @@
  import _ from 'lodash';
+ import './style.css';
 
  function component() {
      var element = document.createElement('div');
      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+     element.classList.add('hello');
      return element;
  }
 
--- webpack官方Demo/webpack-demo/webpack.config.js
+++ webpack官方Demo/webpack-demo/webpack.config.js
@@ -1,9 +1,18 @@
 var path = require('path');
 
 module.exports = {
-	entry:'./src/index.js',
-	output:{
-		filename:'bundle.js',
-		path:path.resolve(__dirname,'dist')
-	}
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
+    }
 };
