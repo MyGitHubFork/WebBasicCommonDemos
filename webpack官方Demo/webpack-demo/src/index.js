bed6f453972e9c0b3cc8284d539bc8860413c55e
xibeifeng <394042583@qq.com>
2017-07-06 10:54:06 +0800

    添加serve实时调试

--- webpack官方Demo/webpack-demo/src/index.js
+++ webpack官方Demo/webpack-demo/src/index.js
@@ -1,26 +1,8 @@
- import _ from 'lodash';
- import './style.less';
- import './style.css';
- import Icon from './icon.png';
- import Data from './data.xml';
+
 
  function component() {
      var element = document.createElement('div');
-
-     // Lodash，现在由此脚本导入
-     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-     //添加样式表
-     element.classList.add('hello');
-     element.classList.add('lessFont');
-
-
-     console.log(Data);
-     alert(Data);
-
-     // 将图像添加到我们现有的 div。
-     // var myIcon = new Image();
-     // myIcon.src = Icon;
-     // element.appendChild(myIcon);
+     element.innerHTML="黄成都";
 
      return element;
  }
